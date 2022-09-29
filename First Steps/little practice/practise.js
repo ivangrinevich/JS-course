a301d8e0e1044028477b0b7a8da5f2fc08fad7c2
Ivanu <ivanushka20032502@gmail.com>
2022-09-30 02:23:30 +0300

    Delete comments

--- First Steps/little practice/practise.js
+++ First Steps/little practice/practise.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// if (1) {
-//     console.log('Ok!');
-// }else {
-//     console.log('Error');
-// }
+if (1) {
+    console.log('Ok!');
+}else {
+    console.log('Error');
+}
 
-// const num = 50;
+const num = 50;
 
-// if (num < 49) {
-//     console.log('Error');
-// }else if (num > 100) {
-//     console.log('Too much');
-// } else {
-//     console.log('Ok!');
-// }
+if (num < 49) {
+    console.log('Error');
+}else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('Ok!');
+}
 
-// (num === 50) ? console.log('Ok!') : console.log('Error');
+(num === 50) ? console.log('Ok!') : console.log('Error');
 
 const num = '50';
 
