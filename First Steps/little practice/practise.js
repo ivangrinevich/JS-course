59b670f032a6f706d601dcc8fe2d2c30d0f442c5
Ivanu <ivanushka20032502@gmail.com>
2022-10-02 15:05:28 +0300

    POP essentials

--- First Steps/little practice/practise.js
+++ First Steps/little practice/practise.js
@@ -1,103 +1,29 @@
 "use strict";
 
-let a = 5,
-    b = a;
+let str = "some";
+let strObj = new String(str);
 
-b = b + 5;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-console.log(b);
-console.log(a);
+console.dir([1, 2, 3]);
 
-const obj = {
-    a: 5,
-    b: 1
-};
-
-// const copy = obj;
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
     }
 };
 
-const newNumbers = copy(numbers);
+const john = Object.create(soldier);
 
-newNumbers.a = 10;
-newNumbers.c.x = 10;
+// const john = {
+//     health: 100
+// };
 
-console.log(newNumbers);
-console.log(numbers);
+// // john.__proto__ = soldier; // old version
 
-const add = {
-    d: 17,
-    e: 20
-};
+// Object.setPrototypeOf(john, soldier); // new version
 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'asdasd';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ["a", "b"];
-
-const newAarray = [...array];
-newAarray[1] = "c";
-
-console.log(array);
-console.log(newAarray);
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
-newObj.one = "one";
-
-console.log(newObj);
-console.log(q);
+john.sayHello();
