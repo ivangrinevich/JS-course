1736e5a88ac74e82f147ba219141a24e1f0d2a1f
Ivanu <ivanushka20032502@gmail.com>
2022-10-02 18:43:56 +0300

    Dynamic typing

--- First Steps/little practice/practise.js
+++ First Steps/little practice/practise.js
@@ -1,23 +1,53 @@
 "use strict";
 
-function hello() {
-    console.log("Hello World");
-    debugger;
+// To string
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontsize = 26 + 'px';
+
+// To Number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(Number(+'5')));
+
+// 3)
+console.log(typeof(parseInt("15px", 10)));
+
+
+let answer = +prompt("Hello", "");
+
+// To boolean
+
+// 0, '', null, indefined, NaN;
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
 }
 
-hello();
+switcher = 1;
 
-function hi() {
-    console.log("Say hi");
+if (switcher) {
+    console.log('Working...');
 }
 
-hi();
+// 2)
+console.log(typeof(Boolean('4')));
 
-const arr = [1, 14, 4, 30, 54],
-      sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-    return a - b;
-} 
-
-console.log(sorted);
+// 3)
+console.log(typeof(!!"44444"));
