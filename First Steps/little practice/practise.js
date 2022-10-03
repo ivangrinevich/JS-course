c6edcedc632c0b9a66def13180028949bbcc6d9a
Ivanu <ivanushka20032502@gmail.com>
2022-10-03 09:47:19 +0300

    Getting elements from page

--- First Steps/little practice/practise.js
+++ First Steps/little practice/practise.js
@@ -1,33 +1,21 @@
 "use strict";
 
-// let x = 5;
-// alert(x++);
+const box = document.getElementById('box');
 
-// console.log([] + false - null + true); // Nan
+console.log(box);
 
-// let y = 1;
-// let x = y = 2;
-// alert(x);
+const btns = document.getElementsByTagName('button');
 
-// console.log(typeof([] + 1 + 2)); // "12"
+console.log(btns[0]);
 
-// alert("1"[0]);
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// console.log(2 && 1 && null && 0 && undefined);
-// И запинается на лжи
-// ИЛИ запинается на правде
+const hearts = document.querySelectorAll('.heart');
 
-// console.log(!!(1 && 2) === (1 && 2));
+hearts.forEach(item => {
+    console.log(item);
+});
 
-// alert(null || 2 && 3 || 4);
-
-// const a = [1, 2, 3];
-// const b = [1, 2, 3];
-
-// console.log(a == b);
-
-// alert(+"Infinity");
-
-// console.log("Ёжик" >= "яблоко");
-
-console.log(0 || "" || 2 || undefined || true || false);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
