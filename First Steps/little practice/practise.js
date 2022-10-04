17de70826cd6adad30ffaac483b179bdc2e5cc38
Ivanu <ivanushka20032502@gmail.com>
2022-10-04 08:32:23 +0300

    DOM navigation and data-attributes

--- First Steps/little practice/practise.js
+++ First Steps/little practice/practise.js
@@ -1,39 +1,22 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-// btn.onclick = function() {
-//     alert('Second click');
-// };
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// let i = 0;
-const deleteElement = (event) => {
-    // event.target.remove();
-    // console.log('Hover');
-    console.log(event.currentTarget);
-    console.log(event.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
 
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+    console.log(node);
+}
