059f22c88071e8b61e3da78be783ca07c753bdd9
Ivanu <ivanushka20032502@gmail.com>
2022-10-06 08:24:51 +0300

    Adding tabs

--- Javascript at work/js/script.js
+++ Javascript at work/js/script.js
@@ -1,43 +1,52 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[1].classList.add('red'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-// if (btns[1].classList.contains('red')) {
-//     console.log('red');
-// }
-
-btns[0].addEventListener('click', () => {
-    // Preferred method
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');    
-    // }
-
-    btns[1].classList.toggle('red');
-});
-
-console.log(btns[0].className);
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.matches('button.red')) {
-        console.log("Hello");
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
     }
+    
+    function showTabContent(i = 0) {
+        // tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+            // let i = 0;
+            // for (let key of tabs) {
+            //     if (target == key) {
+            //         hideTabContent();
+            //         showTabContent(i);
+            //         break;
+            //     }
+            //     i++;
+            // }
+        }
+    });
 });
-
-// btns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         console.log('Hello');
-//     });
-// });
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
